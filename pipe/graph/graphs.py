173beb99e7dd9ec6b489b45a5b593fc60a2a88cb
Richard Roberts <richard.andrew.roberts@gmail.com>
2019-05-28 18:29:29 +1200

    Implement API for graph

--- pipe/graph/graphs.py
+++ pipe/graph/graphs.py
@@ -11,10 +11,25 @@ class BasicGraph:
         self.nodes = {}
         self.edges = []
 
+    def add_node(self, node):
+        self.nodes[node.node_id] = node
+        return node
+
     def new_node(self, path):
         node = nodes.BasicNode(self.lib, path)
         self.nodes[id(node)] = node
         return node
+
+    def list_nodes(self):
+        return [n.as_json() for n in self.nodes.values()]
+    
+    def list_edges(self):
+        return [e.as_json() for e in self.edges]
+
+    def get_node(self, id):
+        if id not in self.nodes.keys():
+            raise KeyError("No known node with id=%s" % str(id))
+        return self.nodes[id]
 
     def connect(self, node_from, arg_from, node_to, arg_to):
         edge = edges.BasicDirectedEdge(
@@ -22,6 +37,12 @@ class BasicGraph:
             node_to, arg_to
         )
         self.edges.append(edge)
+
+    def connect_by_id(self, id_from, arg_from, id_to, arg_to):
+        self.connect(self.get_node(id_from), arg_from, self.get_node(id_to), arg_to)
+
+    def assign_argument(self, id, name, value):
+        self.get_node(id).set_argument(name, value)
 
     def execute_to_root(self, current_node):
         # Evaluate toward root first
@@ -38,4 +59,3 @@ class BasicGraph:
 
         # Then evaluate current
         current_node.evaluate()
-
