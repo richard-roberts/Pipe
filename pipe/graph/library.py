b928be55fa167100857de66f021a45f38d3e4c25
Richard Roberts <richard.andrew.roberts@gmail.com>
2019-06-07 12:53:03 +1200

    Simplify template library interface [note]
    - templates are now simply overwritten if they already exist

--- pipe/graph/library.py
+++ pipe/graph/library.py
@@ -14,36 +14,22 @@ class Library:
     def is_valid_path(self, path):
         return path in self.templates.keys()
 
-    def add(self, path, template):
-        if self.is_valid_path(path):
-            raise ValueError(
-                f"There is already a template saved under `{path}` (use `replace` instead)"
-            )
-        self.templates[path] = template
-
-    def new_basic_template(self, path, args, outs, extension, code):
+    def create_or_update_basic_template(self, path, args, outs, extension, code):
         template = templates.from_data(args, outs, extension, code)
-        self.add(path, template)
+        self.templates[path] = template
         return template
 
-    def new_file_template(self, path, filepath):
+    def create_or_update_file_template(self, path, filepath):
         template = templates.FileTemplate(filepath)
-        self.add(path, template)
-        return template
-    
-    def replace(self, path, template):
-        if not self.is_valid_path(path):
-            raise ValueError(
-                f"There is no template saved under `{path}` (use `add` instead)"
-            )
         self.templates[path] = template
+        return template
 
     def remove(self, path):
         if not self.is_valid_path(path):
             raise ValueError(f"There is no template saved under `{path}`")
         del self.templates[path]
 
-    def move(self, old_path, new_path):
+    def rename_template(self, old_path, new_path):
         if not self.is_valid_path(old_path):
             raise ValueError(
                 f"There is no template saved under `{old_path}`"
@@ -52,7 +38,8 @@ class Library:
             raise ValueError(
                 f"There is already a template saved under `{new_path}`"
             )
-        self.add(new_path, self.get(old_path))
+
+        self.templates[new_path] = self.get(old_path)
         self.remove(old_path)
 
     def get(self, path):
@@ -86,7 +73,7 @@ def from_json(data):
     for path in data.keys():
         template_json = data[path]
         template = templates.from_json(template_json)
-        l.add(path, template)
+        l.templates[path] = template
     return l
 
 def load_interal():
